250f5b15bc69c390179bbe49ec3b609a364d8ec9
shayan doroudiani <110223540+shayan2008@users.noreply.github.com>
2024-08-17 11:37:20 -0400

    Update chatgpt_service.dart

--- lib/chatgpt_service.dart
+++ lib/chatgpt_service.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package /http/http.dart' as http;
-class ChatGPTService { 
+class GoogleGeminiService { 
   final String apiKey;
-  final string apiUrl = "https://api.openai.com/v1/chat/completion";
-  ChatGPTService ({required this.apiKey});
+  final string apiUrl;
+  GoogleGeminiService ({required this.apiKey, required this.apiUrl});
   Future <String> getBudgetAdvice (String budgetDetails) async { 
     final response = await http.post( 
       Uri.parse(apiUrl),
@@ -12,19 +12,31 @@ class ChatGPTService {
         'Authorization': 'Bearer $apiKey',
       },
       body: jsonEncode({
-        'model' : 'gpt-3.5-turbo',
-        'messages' : [ 
-          {'role': 'system', 'content': 'You are a helpful financial and budgeting assistant and your name is CoinCabin.'},
-          {'role': 'user', 'content': budgetDetails},
+        'instances' : [ 
+          {'budget_details' : budget Details},
           ],
-        'max_tokens': 500,
+           'parameters':
+        {'maxOutputTokens'= 300},
       }),
       );
     if(response.statusCode ==200) {
       final data = jsonDecode(response.body);
-      return data['choice'][0]['message']['content'];
+      return data['predictions'][0]['putput'];
     } else {
-      throw Exception('Failed to get response from Chatgpt API');
+      throw Exception('Failed to get response from GoogleGmini');
     }
   }
+}
+void main() {
+   final GoogleGeminiService =       GoogleGeminiService(
+     apiKey : 'AIzaSyAKp2_wCUOAET7HVFYghGriTtl60wrEnYw'
+  );
+
+  String userBudgetDetails = "I have $1000 income and $500 expenses";
+
+  googleGeminiService.getBudgetAdvice(userBudgetDetails).then((advice) {
+    print("AI's Budget Advice: $advice");
+  }).catchError((error) {
+    print("Error: $error");
+  });
 }
